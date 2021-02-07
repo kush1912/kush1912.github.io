01515e9a5355de58008a1f4986aef8779a4aa4b1
kush1912 <vidyajaykushwaha@gmail.com>
2021-02-07 16:11:16 +0530

    Freelancer + Our Work Section Added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Jquery Used for initialising the slick
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:"dots"
+    })
+});
